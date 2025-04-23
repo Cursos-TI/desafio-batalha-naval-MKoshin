c1d87791d285df06e98891f41ea8a9f006082f01
Marco Antonio <94036718+MKoshin@users.noreply.github.com>
2025-04-23 11:12:51 -0300

    Update batalhaNaval.c
    Inicio campoNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+int main(){
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    /*NAVIO VERTICAL: TAMANHO 3  [8,3] [8,4] [8,5]
+      NAVIO VERTICAL: TAMANHO 4  [1,4] [1,5] [1,6] [1,7]
+      NAVIO VERTICAL: TAMANHO 2  [6,3] [6,4]
+      NAVIO HORIZONTAL: TAMANHO 2 [4,2] [5,2]
+      NAVIO HORIZONTAL: TAMANHO 3 [2,0] [3,0] [4,0]
+      NAVIO HORIZONTAL: TAMANHO 4 [3,9] [4,9] [5,9] [6,9]
+      */
+    // CAMPO NAVAL
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    int campoNaval[10][10] ={
+      // 0  1  2  3  4  5  6  7  8  9
+        {0, 0, 3, 3, 3, 0, 0, 0, 0, 0}, //0
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //1
+        {0, 0, 0, 0, 2, 2, 0, 0, 0, 0}, //2
+        {0, 0, 0, 0, 0, 0, 2, 0, 3, 0}, //3
+        {0, 4, 0, 0, 0, 0, 2, 0, 3, 0}, //4
+        {0, 4, 0, 0, 0, 0, 0, 0, 3, 0}, //5
+        {0, 4, 0, 0, 0, 0, 0, 0, 0, 0}, //6
+        {0, 4, 0, 0, 0, 0, 0, 0, 0, 0}, //7
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //8
+        {0, 0, 0, 4, 4, 4, 4, 0, 0, 0}  //9
+    };
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // loop para exibição do campo
+    for(int i = 0; i < 10; i++){
+      for(int j = 0; j < 10; j++){
+        printf("%d ", campoNaval[i][j]);
+      }
+      printf(" \n");
+    }
 
     return 0;
 }
