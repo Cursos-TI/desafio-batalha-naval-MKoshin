8f081dc60880b4b755fbd2505c66ec3cd8b08afb
Marco Antonio <94036718+MKoshin@users.noreply.github.com>
2025-04-25 15:16:15 -0300

    Update batalhaNaval.c
    Adicionado:
    ataques CONE - OCTAEDRO - CRUZ
    identificados como 5.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,6 +6,8 @@ int main(){
   // campo final
   char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
+  printf("-------------BATALHA NAVAL-----------\n");
+
     int campoNavalSoma[10][10] = 
     {
       // 0  1  2  3  4  5  6  7  8  9 COLUNAS
@@ -21,6 +23,12 @@ int main(){
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}  //9
     };
 
+
+    // MATRIZES PARA AS HABILIDADES -- CONE // CRUZ // OCTAEDRO
+    int cone[5][5] = {{0, 0, 5, 0, 0}, {0, 5, 5, 5, 0}, {5, 5, 5, 5, 5}};
+    int octaedro[3][3] = {{0, 5, 0}, {5, 5, 5}, {0, 5, 0}};
+    int cruz[1] = {5};
+
     // declarando navio
     int navio[1] = {3};
 
@@ -34,7 +42,6 @@ int main(){
     }
 
     // loop para barco horizontal
-
     for(int i = 6; i < 7; i++)
     {
       for(int j = 5; j < 8; j++)
@@ -44,7 +51,6 @@ int main(){
     }
 
     // Loop para barco diagonal
-
     for(int i = 2; i < 5; i++)
     {
       for(int j = 2; j < 5; j++)
@@ -57,7 +63,6 @@ int main(){
     }
 
     // Loop para barco diagonal inversa
-    
     for(int i = 1; i < 4; i++)
     {
       for(int j = 6; j < 9; j++)
@@ -65,6 +70,43 @@ int main(){
         if(i + j == 9)
         {
         campoNavalSoma[i][j] = navio[0];
+        }
+      }
+    }
+
+    // Loop para o ataque cone área afetada C6, B7 C7 D7, A8 B8 C8 D8 E8;
+    for(int i = 0; i < 5; i++)
+    {
+      for(int j = 0; j < 5; j++)
+      {
+        campoNavalSoma[5+i][j] = cone[i][j];
+      }
+    }
+
+    //loop para octaedro áre afetada H8, G9 H9 I9, H10.
+    for(int i = 0; i < 3; i++)
+    {
+      for(int j = 0; j < 3; j++)
+      {
+        campoNavalSoma[7+i][6+j] = octaedro[i][j];
+      }
+    }
+
+    // loop para a cruz
+    // J coluna I linha
+    // Favor nao ligar para o garrancho um dia eu arrumo perfeitamente <3
+    // cordenadas F1, D2 E2 F2 G2 H2, F3.
+    for(int i = 0; i < 3; i++)
+    {
+      for(int j = 3; j < 8; j++)
+      {
+        if(j == 5)
+        {
+          campoNavalSoma[0][5] = cruz[0];
+          campoNavalSoma[1][5] = cruz[0];
+        } else if(i == 1){
+          campoNavalSoma[i][j] = cruz[0];
+          campoNavalSoma[2][5] = cruz[0];
         }
       }
     }
@@ -80,7 +122,6 @@ int main(){
     printf(" \n");
     
     // loop para exibir o campo naval
-
     for(int i = 0; i < 10; i++)
     {
       if(i <= 8)
